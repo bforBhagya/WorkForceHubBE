bc9ce27a1b2af44afb607075ad65be99c064a2a5
bforbhagya <bhagya.ariyarathne@gmail.com>
2023-12-05 21:25:51 +0530

    first commit

---
+++ EMS.API/Controllers/EmployeesController.cs
@@ -0,0 +1,37 @@
+﻿using EMS.API.Data;
+using EMS.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class EmployeesController : Controller
+    {
+        private readonly EmsDbContext _emsDbContext;
+        public EmployeesController(EmsDbContext emsDbContext)
+        {
+            _emsDbContext = emsDbContext;
+
+        }
+        [HttpGet]
+
+        public async Task<IActionResult> GetAllEmployees()
+        {
+            var employees = await _emsDbContext.Employee.ToListAsync();
+            return Ok(employees);
+        }
+        [HttpPost]
+
+        public async Task<IActionResult> AddEmployee([FromBody] Employee employeeRequest)
+        {
+            employeeRequest.Id = Guid.NewGuid();
+            await _emsDbContext.Employee.AddAsync(employeeRequest);
+            await _emsDbContext.SaveChangesAsync();
+            return Ok(employeeRequest);
+
+
+        }
+    }
+}
---
+++ EMS.API/Data/EmsDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EMS.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace EMS.API.Data
+{
+    public class EmsDbContext : DbContext
+    {
+        public EmsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
